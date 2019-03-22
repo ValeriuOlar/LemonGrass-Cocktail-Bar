f664328cb4b07e243a8cc06c82317711d8216418
Valeriu <ovaleriu7@gmail.com>
2019-03-22 21:12:11 +0000

    update javascript open and css

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 /* Select a class on click event litle corner bar */
 $('.menu').on('click', function () {
     $(this).toggleClass('active');
+    $('.overlay').toggleClass('menu-open');
 });
 /* to add more functions */
